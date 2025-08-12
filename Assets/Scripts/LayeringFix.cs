de0011445fc569abda428183c2e7e164abb58f96
EasilyDistractedInteractive <163216751+EasilyDistractedInteractive@users.noreply.github.com>
2025-08-12 15:24:50 +1000

    Layering Fix Implemented
    Layering Fix Implemented

---
+++ Assets/Scripts/LayeringFix.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LayeringFix : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public GameManager Manager;
+    public Player player;
+    Transform playerTransform;
+    public float yPos;
+    public int orderPos;
+
+    void Start()
+    {
+        Manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();//find gamemanager
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();//find Player     
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if(spriteRenderer == null)
+        {
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();// Get renderer
+        }
+
+        
+    }
+
+    void Update()
+    {
+        Vector2 positionScaledToCamera = transform.position - playerTransform.position;
+        yPos = positionScaledToCamera.y * 32;
+        yPos = Mathf.Round(yPos);
+        orderPos = (int)yPos;
+        spriteRenderer.sortingOrder = orderPos * -1;
+    }
+
+}
